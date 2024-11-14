4b8a22050a22223ed85c23b1b3af6b65f394bac3
gaimerI <gaimeri17@gmail.com>
2024-11-14 15:54:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ function applyTheme() {
 
 function toggleDarkMode() {
   // Toggle dark mode
-  class document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("dark-mode");
   // Update localStorage based on the current mode
   if (document.body.classList.contains("dark-mode")) {
     localStorage.setItem("dark-mode", "enabled");
-  }
-  else { localStorage.setItem("dark-mode", "disabled");
+  } else {
+    localStorage.setItem("dark-mode", "disabled");
   }
 }
 
