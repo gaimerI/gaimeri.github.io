47f2ed6d3c0b86bd3781d9cc59ccadc578ecc70b
gaimerI <gaimeri17@gmail.com>
2024-11-24 20:16:38 +0200

    Update script.js

--- password/script.js
+++ password/script.js
@@ -1,26 +1,36 @@
-const _0x1f5b = "this_is_a_very_long_phrase_that_hopefully_is_not_in_any_dictionary", _0x4c2f = document.getElementById("password-prompt"), _0x5a8e = document.getElementById("protected-content"), _0x2d85 = document.getElementById("password-input"), _0x376f = document.getElementById("submit-button"), _0x1e61 = document.getElementById("error-message"), _0x6a0d = document.getElementById("random-element-1"), _0x1d77 = document.getElementById("random-element-2"), _0x3c5a = document.getElementById("random-element-3"), _0x5b8d = document.getElementById("random-element-4");
+(function(){
+  const _0x8b7f = atob("dGhpcyBpc19hX3ZlcnlfbG9uZ19waGByYXNlX3RoYXRfaG9wZWZ1bGx5X2lzX25vdF9pbl9hbnlfZGljdGlvbmFyeQ=="), 
+        _0x5a4d = document.getElementById("password-prompt"), 
+        _0x1cbf = document.getElementById("protected-content"), 
+        _0x1294 = document.getElementById("password-input"), 
+        _0x439a = document.getElementById("submit-button"), 
+        _0x6a45 = document.getElementById("error-message"), 
+        _0x7f30 = document.getElementById("random-element-1"), 
+        _0x5e73 = document.getElementById("random-element-2"), 
+        _0x62c2 = document.getElementById("random-element-3"), 
+        _0x3c17 = document.getElementById("random-element-4");
 
-_0x376f.addEventListener("click", () => {
-  let _0x7b3e = _0x2d85.value;
-  _0x7b3e = _0x7b3e.trim();
-  let _0x385b = _0x7b3e.length;
-  let _0x47f4 = _0x385b > 0 ? true : false;
-  let _0x4b87 = _0x47f4 && _0x7b3e !== '';
-  let _0x590f = _0x4b87 ? _0x7b3e : null;
+  _0x439a.addEventListener("click", function() {
+    let _0x5e29 = _0x1294.value.trim();
+    let _0x233e = _0x5e29.length;
+    let _0x34a7 = _0x233e > 0 ? true : false;
+    let _0x5810 = _0x34a7 && _0x5e29 !== '';
+    let _0x4b91 = _0x5810 ? _0x5e29 : null;
 
-  if (_0x590f === _0x1f5b) {
-    _0x4c2f.style.display = "none";
-    _0x5a8e.style.display = "block";
-  } else {
-    _0x1e61.style.display = "block";
-  }
+    if (_0x4b91 === _0x8b7f) {
+      _0x5a4d.style.display = "none";
+      _0x1cbf.style.display = "block";
+    } else {
+      _0x6a45.style.display = "block";
+    }
 
-  if (_0x7b3e === '') {
-    _0x6a0d.style.display = "none";
-  } else {
-    _0x1d77.style.display = "inline-block";
-  }
+    if (_0x5e29 === '') {
+      _0x7f30.style.display = "none";
+    } else {
+      _0x5e73.style.display = "inline-block";
+    }
 
-  let _0x2207 = _0x3c5a ? _0x3c5a.style.display = "flex" : '';
-  let _0x6fbb = (_0x7b3e.length % 2 === 0) ? _0x5b8d.style.display = "inline-block" : _0x5b8d.style.display = "none";
-});
+    let _0x14b6 = _0x62c2 ? _0x62c2.style.display = "flex" : '';
+    let _0x4f1b = (_0x5e29.length % 2 === 0) ? _0x3c17.style.display = "inline-block" : _0x3c17.style.display = "none";
+  });
+})();
