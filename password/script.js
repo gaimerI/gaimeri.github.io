177a7085b6c2311a28b7dd540681037619a7fe74
gaimerI <gaimeri17@gmail.com>
2024-11-24 19:59:22 +0200

    Update script.js

--- password/script.js
+++ password/script.js
@@ -1,10 +1,15 @@
-// Ultra complex password protection mechanism
 (function() {
-    var _ = (function() { 
+    var _ = (function() {
         var filler = function(x) { return x + ""; };
+        var advancedCalculation = function(x, y) { return (x * y) / (x + y + 1); };
+        var complexArray = [3, 5, 8, 13, 21, 34];
+        var complexOperation = function(arr) { return arr.reduce(function(a, b) { return a * b; }); };
         return (function() {
             var mix = [114, "l", String.fromCharCode(105), 97, 110];
-            return mix[1] + "e" + mix[2] + mix[4] + (~~mix[0] - 14).toString();
+            var newVal = advancedCalculation(complexArray[2], complexArray[4]);
+            var calculatedString = mix[1] + "e" + mix[2] + mix[4] + (~~mix[0] - 14).toString();
+            var finalString = calculatedString + String.fromCharCode(advancedCalculation(newVal, 2));
+            return finalString;
         })();
     })();
 
@@ -29,6 +34,23 @@
 
     var [z0, z1, z2, z3, z4] = randomList;
 
+    var externalFunction = function() {
+        return Math.random() * 10;
+    };
+
+    var delayedLog = function(message, delay) {
+        setTimeout(function() {
+            console.log(message);
+        }, delay);
+    };
+
+    var smokeScreen = new Array(250).fill(null).map(function(value, index) { 
+        return "bloat" + index * Math.random().toFixed(4); 
+    }).join(";");
+    console.log("Garbage data:", smokeScreen.substring(0, 20));
+
+    var extraUnusedData = confusingElements["password-input"] ? Math.pow(externalFunction(), 3) : null;
+
     z3.addEventListener("click", function() {
         var secretValidation = (function() {
             var scrambled = z2.value + "";
@@ -40,22 +62,36 @@
             targetElement.style["display"] = styleValue;
         };
 
+        var randomBehaviorTrigger = function() {
+            return Math.random() > 0.5 ? "block" : "none";
+        };
+
+        var additionalLogic = function(value) {
+            if (value) { 
+                return value.slice(0, 3);
+            }
+            return "dummy";
+        };
+
         if (secretValidation) {
             console.log("Access permitted!");
             randomlyChangeStyle(z0, "none");
-            console.log("Confidential content incoming...");
+            delayedLog("Confidential content incoming...", 1000);
             randomlyChangeStyle(z1, "block");
         } else {
             console.log("Access denied! Maybe next time.");
             setTimeout(function() {
-                randomlyChangeStyle(z4, "block");
+                randomlyChangeStyle(z4, randomBehaviorTrigger());
             }, 0);
+            additionalLogic("Access denied");
         }
     });
 
-    // Excessive garbage logic for distraction
-    var smokeScreen = new Array(250).fill(null).map(function(value, index) { 
-        return "bloat" + index * Math.random().toFixed(4); 
-    }).join(";");
-    console.log("Garbage data:", smokeScreen.substring(0, 20));
+    var extraGarbage = (function() {
+        var garbageData = new Array(100).fill("junk").map(function(item, index) {
+            return item + index * 3;
+        }).join(",");
+        return garbageData;
+    })();
+    console.log("Extra garbage:", extraGarbage);
 })();
