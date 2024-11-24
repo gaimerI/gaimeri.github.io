fa8887eb8e5324e7fe41d5be65c376d5131b7e89
gaimerI <gaimeri17@gmail.com>
2024-11-24 20:57:34 +0200

    Update script.js

--- password/script.js
+++ password/script.js
@@ -1,5 +1,5 @@
 // Define the correct password
-const correctPassword = "thisisaverylongphrasethathopefullyisnotinanydictionary";
+const correctPassword = "secure123";
 
 // Get references to the HTML elements
 const passwordPrompt = document.getElementById("password-prompt");
